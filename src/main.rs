bdb6bd71c7f85d261ccf09b05cee466d7f59eee9
_ <ReactorScram@users.noreply.github.com>
2024-09-17 14:51:48 +0000

    replicate https://github.com/firezone/firezone/issues/6704

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use atomicwrites::{AtomicFile, OverwriteBehavior};
+
+fn main() {
+    let path = "nothing.txt";
+    
+    std::fs::remove_file(path).ok();
+    let file = AtomicFile::new(path, OverwriteBehavior::DisallowOverwrite);
+    file.write(|_| Ok::<_, std::io::Error>(())).unwrap();
+}
